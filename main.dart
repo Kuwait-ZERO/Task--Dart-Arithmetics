75446fa1095681d965d4c3fe260e25c10513c62c
Kuwait-zero <hussein-_2000@hotmail.com>
2024-09-30 03:30:57 -0400

    final

--- main.dart
+++ main.dart
@@ -1,10 +1,13 @@
 void main() {
-  double tempInCilsius = 30;
-  double tempInFahrenheit = 86;
-  double fahrenheitToCelsius= ((tempInFahrenheit -32)/1.8);
-  double celsiusToFahrenhit= (tempInCilsius * 1.8000 + 32.00);
+  double tempInCelsius = 30;
+  double tempInFahrenheit = 83;
+  double fahrenheitToCelsius = ((tempInFahrenheit - 32) / 1.8);
+  double celsiusToFahrenheit = (tempInCelsius * 1.8000 + 32.00);
+  
+  // Format to show at most 1 fractional digit
+  String formattedFahrenheitToCelsius = fahrenheitToCelsius.toStringAsFixed(1);
+  String formattedCelsiusToFahrenheit = celsiusToFahrenheit.toStringAsFixed(1);
 
-  print( "$tempInFahrenheit F is $fahrenheitToCelsius C");
-  print( "$tempInCilsius C is $celsiusToFahrenhit F");
-
+  print("$tempInFahrenheit F is $formattedFahrenheitToCelsius C");
+  print("$tempInCelsius C is $formattedCelsiusToFahrenheit F");
 }
